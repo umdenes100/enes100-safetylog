a3b3f67201096bf68abe5fa5e9ac7dcda9c19f2b
cmwharff <115747231+cmwharff@users.noreply.github.com>
2025-09-12 14:26:46 -0400

    setup commit

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { hide, show } from "./showhide.js"; // Import the functions
+import { child, get, ref, set, runTransaction } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-database.js";
+import { database } from './firebaseConfig.js';
+
+// Functions to define:
